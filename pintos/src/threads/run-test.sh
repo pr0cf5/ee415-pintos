eff561c9787bc20b9b09e67f19f229f17d35ccef
pr0cf5 <wireless@kaist.ac.kr>
2022-03-06 16:28:54 +0000

    make testing script print stdout for initial run

--- pintos/src/threads/run-test.sh
+++ pintos/src/threads/run-test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd build
+pintos --qemu  -- -q  run $1
 pintos --qemu  -- -q  run $1 < /dev/null 2> tests/threads/$1.errors > tests/threads/$1.output
 perl -I../.. ../../tests/threads/$1.ck tests/threads/$1 tests/threads/$1.result
