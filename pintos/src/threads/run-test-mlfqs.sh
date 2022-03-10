2490e7a2a39e58a630cf28c9a9a9dcf1754059f2
pr0cf5 <wireless@kaist.ac.kr>
2022-03-10 15:30:42 +0000

    fix scripts so that test isn't ran twice

--- pintos/src/threads/run-test-mlfqs.sh
+++ pintos/src/threads/run-test-mlfqs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd build
-pintos --qemu -- -q  run $1 < /dev/null 2> tests/threads/$1.errors > tests/threads/$1.output
+pintos --qemu -- -q  -mlfqs run $1 < /dev/null 2> tests/threads/$1.errors > tests/threads/$1.output
 cat tests/threads/$1.output
 perl -I../.. ../../tests/threads/$1.ck tests/threads/$1 tests/threads/$1.result
